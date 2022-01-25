f137f8dbf5153fb91bb0acbd35c9bd77bb80e1ee
Falk Markus Dursun Jetlund <falk@jetlund.com>
2022-01-25 14:03:34 +0100

    Refactored main.js

--- src/bin/main.js
+++ src/bin/main.js
@@ -21,63 +21,60 @@ const { renderToStaticMarkup } = require("react-dom/server");
 const reactIs = require("react-is");
 const mapAsync = (array, func) => Promise.all(array.map(func));
 
-const publicDir = process.env.PUBLIC_DIR || "./public";
-
-const locals = async ctx => {
-  return {
-    redirect: path => {
-      throw new response.Redirect(path);
-    },
-    response,
-    ctx,
-    elements,
+const createApp = async ({ publicDir, mongoURI, keys }) => {
+  const locals = async ctx => {
+    return {
+      redirect: path => {
+        throw new response.Redirect(path);
+      },
+      response,
+      ctx,
+      elements,
+    };
   };
-};
 
-const ejsHandler = filePath => async ctx => {
-  ctx.set("Content-Type", "text/html");
-  const options = { root: resolve(publicDir), async: true };
-  const l = await locals(ctx);
-  const html = await ejs.renderFile(filePath, l, options);
-  ctx.body = html;
-};
+  const ejsHandler = filePath => async ctx => {
+    ctx.set("Content-Type", "text/html");
+    const options = { root: resolve(publicDir), async: true };
+    const l = await locals(ctx);
+    const html = await ejs.renderFile(filePath, l, options);
+    ctx.body = html;
+  };
 
-const registerFile = async (dir, file, router) => {
-  let [path, extension] = file.split(/\.(?=[^.]+$)/);
-  path = path === "index" ? "/" : `/${path}`;
-  const filePath = join(dir, file);
-  if (extension === "ejs") router.all(path, ejsHandler(filePath));
-  else if (extension === "jsx") {
-    const module = require(resolve(filePath));
-    const subRouter = new Router({ prefix: `${path}` });
-    if (module.get) subRouter.get("/", module.get);
-    if (module.post) subRouter.post("/", module.post);
-    if (module.put) subRouter.put("/", module.put);
-    if (module.patch) subRouter.patch("/", module.patch);
-    if (module.delete) subRouter.delete("/", module.delete);
-    if (module.all) subRouter.all("/", module.all);
+  const registerFile = async (dir, file, router) => {
+    let [path, extension] = file.split(/\.(?=[^.]+$)/);
+    path = path === "index" ? "/" : `/${path}`;
+    const filePath = join(dir, file);
+    if (extension === "ejs") router.all(path, ejsHandler(filePath));
+    else if (extension === "jsx") {
+      const module = require(resolve(filePath));
+      const subRouter = new Router({ prefix: `${path}` });
+      if (module.get) subRouter.get("/", module.get);
+      if (module.post) subRouter.post("/", module.post);
+      if (module.put) subRouter.put("/", module.put);
+      if (module.patch) subRouter.patch("/", module.patch);
+      if (module.delete) subRouter.delete("/", module.delete);
+      if (module.all) subRouter.all("/", module.all);
 
-    router.use(subRouter.routes());
-  }
-};
-
-const registerDir = async (dir, router) => {
-  const isHidden = fn => fn.startsWith(".") || fn.startsWith("_");
-  const files = (await readdir(dir)).filter(fn => !isHidden(fn));
-  await mapAsync(files, async file => {
-    if (file.startsWith("_") || file.startsWith(".")) return;
-    if ((await stat(join(dir, file))).isDirectory()) {
-      const subRouter = new Router({ prefix: `/${file}` });
-      await registerDir(join(dir, file), subRouter);
       router.use(subRouter.routes());
     }
-    await registerFile(dir, file, router);
-  });
-};
+  };
 
-async function main() {
-  const mongo =
-    process.env.MONGODB_URI && new MongoClient(process.env.MONGODB_URI);
+  const registerDir = async (dir, router) => {
+    const isHidden = fn => fn.startsWith(".") || fn.startsWith("_");
+    const files = (await readdir(dir)).filter(fn => !isHidden(fn));
+    await mapAsync(files, async file => {
+      if (file.startsWith("_") || file.startsWith(".")) return;
+      if ((await stat(join(dir, file))).isDirectory()) {
+        const subRouter = new Router({ prefix: `/${file}` });
+        await registerDir(join(dir, file), subRouter);
+        router.use(subRouter.routes());
+      }
+      await registerFile(dir, file, router);
+    });
+  };
+
+  const mongo = mongoURI && new MongoClient(mongoURI);
   if (mongo) await mongo.connect();
   const mongodb = mongo && mongo.db();
 
@@ -87,9 +84,8 @@ async function main() {
     return next();
   });
 
-  const key =
-    "FNLR9WBXnqojIt6I/kMXg9KU0mTKLH6MUuaL++bEYXVnSECuUkEfNUskUpctGCUQ9kK5QbD56zXZs70fuY0U+Q==";
-  app.keys = [key];
+  if (Array.isArray(keys)) app.keys = keys;
+  else app.keys = [keys];
   const imgExtensions = ["jpeg", "jpg", "svg", "png", "webp", "ico"];
   const extensions = ["js", "css", "html", ...imgExtensions];
   app.use(serve(publicDir, { extensions }));
@@ -118,10 +114,17 @@ async function main() {
     }
   });
   await registerDir(publicDir, router);
-  router.get("/help", ctx => (ctx.body = "body"));
-
   app.use(router.routes(), router.allowedMethods());
 
+  return app;
+};
+
+async function main() {
+  const app = await createApp({
+    publicDir: process.env.PUBLIC_DIR || "./public",
+    mongoURI: process.env.MONGODB_URI,
+    keys: process.env.KEYS.split(":"),
+  });
   const port = Number.parseInt(process.env.PORT) || 3000;
   await app.listen(port);
   console.log(`listening on http://localhost:${port}/`);
